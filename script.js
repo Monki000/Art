759e9a8d4118d5ad44caf5c628965b1e3cf0eef9
Monki000 <monkyingbusiness@gmail.com>
2024-10-26 15:20:52 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic JavaScript for interactivity
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Welcome to My Art Store!");
+    // You can add more interactive features here
+});
